18672cafc3cc21112e16fdc22336b5ae3cf5a333
FremontTeng <fremontteng@gmail.com>
2020-03-03 18:39:01 +0800

    Added status popup to play screen

---
+++ menus/util/OutcomeDisplay.gd
@@ -0,0 +1,4 @@
+extends VBoxContainer
+
+func _on_CloseButton_pressed():
+	self.visible = false
---
+++ scripts/interface/AnswerQuestion.gd
@@ -0,0 +1,5 @@
+extends Control
+
+#Check if correct answer was click
+func _on_AnswerButton_pressed():
+	$DisplayStatus.visible = true
