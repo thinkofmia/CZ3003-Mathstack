d6b5844ae57a2fcbba529151a08d62e516b4c90b
FremontTeng <fremontteng@gmail.com>
2020-03-07 10:11:02 +0800

    Updated time and highscore in gameover screen

--- scripts/logic/GameOver.gd
+++ scripts/logic/GameOver.gd
@@ -2,8 +2,7 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#Set time
-	$Header/RichTextLabel.set_text("Gameover")
 	#Set Highscore
 	$PlayBoard/HighscoreRow/Score.set_text(str(global.highscore))
+	#Set Time
 	$PlayBoard/TimeElapsedRow/Time.set_text(str(global.time))
