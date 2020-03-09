8e26d95f0a6221f6e19edb2e6e0e0b8072b07edf
FremontTeng <fremontteng@gmail.com>
2020-03-09 16:35:31 +0800

    Fix normal mode play to preview

--- scripts/interface/NormalMode.gd
+++ scripts/interface/NormalMode.gd
@@ -7,6 +7,13 @@ func _ready():
 	pass # Replace with function body.
 
 
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_QuitButton_pressed():
+	get_tree().change_scene("res://menus/gameModes/NormalModePreview.tscn")
+	pass # Replace with function body.
