3be20c0cfe1e72e219006954b8667525d7a26447
sweesenkoh <sweesenworkstation@gmail.com>
2020-03-18 22:47:58 +0800

    added some logic to show screen after normal mode game ended

--- Control/interface/NormalMode.gd
+++ Control/interface/NormalMode.gd
@@ -3,11 +3,13 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#Set Header
+	global.questionCount = 0
 	$Header/RichTextLabel.set_text(global.worldSelected+": "+global.difficultySelected)
 	changeBg(global.worldSelected.split("#")[1])
 	currentDelta = -1
 	times = 0
 	currentStoryScore = global.storyScore
+	currentQuestionCount = 1
 	pass # Replace with function body.
 
 func changeBg(selectedBg):
@@ -28,22 +30,37 @@ func changeBg(selectedBg):
 var currentStoryScore = global.storyScore
 var currentDelta = -1
 var times = 0
+var currentQuestionCount = 1
 
 func _process(delta):
 	
-	if (currentDelta == delta):
-		if (times == 20):
-			$CharacterSprite.play("idle")
-			currentDelta = -1
-			times = 0
-		else:
-			times = times + 1
+	if (global.questionCount == 10):
+		doGameEndedProcess()
 	
-	if (currentStoryScore != global.storyScore):
-		currentDelta = delta
-		$CharacterSprite.play("attack")
-		currentStoryScore = global.storyScore
+	else:
+		if (currentDelta == delta):
+			if (times == 20):
+				$CharacterSprite.play("idle")
+				currentDelta = -1
+				times = 0
+			else:
+				times = times + 1
+	
+		if (currentStoryScore != global.storyScore):
+			currentDelta = delta
+			$CharacterSprite.play("attack")
+			currentQuestionCount = currentQuestionCount + 1
+			currentStoryScore = global.storyScore
+	
 
+
+func doGameEndedProcess():
+	$QuestionMenu.hide()
+	$FinishLabel.text = "Congratulations!" if currentStoryScore >= 8 else "Sorry. Please try again!"
+	$FinishLabel.show()
+	$FinishButton.text = "Next Level" if currentStoryScore >= 8 else "Try again"
+	$FinishButton.show()
+	pass
 
 func _on_QuitButton_pressed():
 	get_tree().change_scene("res://View/gameModes/NormalModeSelectDifficulty.tscn")
--- Control/interface/NormalModeSelectWorld.gd
+++ Control/interface/NormalModeSelectWorld.gd
@@ -5,16 +5,19 @@ extends Node
 # var a = 2
 # var b = "text"
 var currentSelectedWorld = "World #0"
+onready var http : HTTPRequest = $HTTPRequest
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	global.modeSelected = "Normal Mode"
+	
 	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if global.worldSelected != currentSelectedWorld:
+		Firebase.save_document("normalmodeprogress",{"progress":{"integerValue":6}}, http)
 		changeBg(global.worldSelected.split("#")[1])
 		currentSelectedWorld = global.worldSelected
 		$NextButton/NextButtonLabel.text = "Enter " + global.worldSelected
