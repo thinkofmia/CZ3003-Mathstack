5d70f937319df00d33b20bad50eb7715d3b6e5c9
sweesenkoh <sweesenworkstation@gmail.com>
2020-04-11 18:31:54 +0800

    statistics normal screen now show average score out of 30. Pls check

--- Control/statistics/StatisticScene.gd
+++ Control/statistics/StatisticScene.gd
@@ -9,6 +9,8 @@ onready var http : HTTPRequest = $HTTPRequest
 onready var http2 : HTTPRequest = $HTTPRequest2
 var totalNumOfStudents = -1
 var totalNumOfCompletions = -1
+var totalScoreCompletions = -1
+var totalScoreAverage = -1
 
 
 # Called when the node enters the scene tree for the first time.
@@ -49,13 +51,22 @@ func _on_HTTPRequest2_request_completed(result, response_code, headers, body):
 		#success
 		200:
 			totalNumOfCompletions = 0
+			totalScoreCompletions = 0
 			var students = ((response_body['documents']))
 			for i in range(0,len(students)):
 				var resultNum = (students[i]['fields']['World' + str(global.statWorldSelected)]['stringValue'])
 				var resultNumInt = int(resultNum)
 				if (resultNumInt == 3):
 					totalNumOfCompletions = totalNumOfCompletions + 1
+				
+				var totalScoreNum = int(students[i]['fields']['ScoreWorld' + str(global.statWorldSelected) + 'a']['stringValue'])
+				totalScoreNum = totalScoreNum + int(students[i]['fields']['ScoreWorld' + str(global.statWorldSelected) + 'b']['stringValue'])
+				totalScoreNum = totalScoreNum + int(students[i]['fields']['ScoreWorld' + str(global.statWorldSelected) + 'c']['stringValue'])
+				
+				totalScoreCompletions = totalNumOfCompletions + totalScoreNum
 			
+			totalScoreAverage = float(totalScoreCompletions) / float(len(students))
+			$TextureRect/MarginContainer/MarginContainer/VBoxContainer/GridContainer/AverageLabel.set_text(str(totalScoreAverage) + "/30")
 			if (totalNumOfStudents != -1):
 				var percentage = float(totalNumOfCompletions) / float(totalNumOfStudents)
 				percentage = percentage * 100
