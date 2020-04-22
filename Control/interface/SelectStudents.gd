3ac07e44239983fec7097dcee4789b9e483bfe9a
FremontTeng <fremontteng@gmail.com>
2020-04-22 14:48:41 +0800

    Fixed bugs between students and questions

--- Control/interface/SelectStudents.gd
+++ Control/interface/SelectStudents.gd
@@ -8,34 +8,43 @@ var student_info = []
 var student_display
 var getStudents=false
 var getQuiz=false
-var qnList
+var studentList
 
-var newButton = load("res://Model/buttons/interface/editQnButtons.tscn")
+var newButton = load("res://Model/buttons/interface/userButtons.tscn")
+
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#Set Header
-	$TemplateScreen/Header.set_text(global.worldSelected+": List of Questions")
+	#Set Target type as Student
+	global.targetType = "Student"
 	#Save Node as student List
-	qnList = $PlayBoard/ScrollContainer/qnList
+	studentList = $PlayBoard/ScrollContainer/ListOfStudents
 	
-	####Requires firebase	
-	#Total Number of Questions
-	var totalQuestions = 5
-	#Loop based on No of questions 
-	for i in range (0,totalQuestions):
-		#Get Qn Name
-		var qnName = "What is 1+"+str(i)+"?" 
-		#Add new instance
-		var addButton = newButton.instance()
-		#Change button name to quiz name
-		addButton.set_text(qnName)
-		#Add qn button to the list
-		qnList.add_child(addButton)
-
+	####Requires firebase
+	getStudents=true
+	Firebase.get_document("users", http)
+	yield(get_tree().create_timer(5.0), "timeout")
+	#Total Number of Teachers
+	#var totalNoUsers = 5
+	student_info = (students.values())
+	var totalNoUsers = student_info[0].size()
+	#Loop based on No of students 
+	for i in range (0,totalNoUsers):
+		#extract question attribute based on i
+		student_display= (student_info[0][i]['fields'])
+		print(str(student_display['account'].values()[0]))
+		if student_display['account'].values()[0] == "Student":
+				#Add new instance
+				var addButton = newButton.instance()
+				#Change button name to quiz name
+				addButton.set_text(str(student_display['nickname'].values()[0]))
+				#Add quiz button to the list
+				studentList.add_child(addButton)
 
 
 func _on_BackButton_pressed():
-	get_tree().change_scene("res://View/teachers/AddQnsSelectWorld.tscn")
+	get_tree().change_scene("res://View/admin/SelectUserType.tscn")
 
 
 func _on_HTTPRequest_request_completed(result: int, response_code: int, headers: PoolStringArray, body: PoolByteArray) -> void:
@@ -49,7 +58,3 @@ func _on_HTTPRequest_request_completed(result: int, response_code: int, headers:
 			self.students = response_body
 			
 			
-
-
-func _on_AddButton_pressed():
-	get_tree().change_scene("res://View/teachers/AddQnsDetails.tscn")
