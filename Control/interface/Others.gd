1775eeab0dcc2397488addef885f63245bb648f5
FremontTeng <fremontteng@gmail.com>
2020-04-24 09:12:19 +0800

    Added others selection interface

---
+++ Control/interface/Others.gd
@@ -0,0 +1,24 @@
+extends Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+#Brings user to main menu according to account type
+func goToMainMenu():
+	if (global.accountType == "Teacher"|| global.accountType == "Admin"): #If account type is teacher or admin
+		get_tree().change_scene("res://View/Screens_Randy/MainMenuTeachers.tscn")
+	else:#If account type is student
+		get_tree().change_scene("res://View/Screens_Randy/MainMenu.tscn")
+
+
+func _on_BackButton_pressed():
+	goToMainMenu() #Return to main menu
+
+#Go to settings Screen
+func _on_SettingsButton_pressed():
+	get_tree().change_scene("res://View/Screens_Randy/SettingsScreen.tscn")
+
+#Go to lore screen
+func _on_LoreButton_pressed():
+	pass # Replace with function body.
--- Control/randy_scripts/SettingsScreen.gd
+++ Control/randy_scripts/SettingsScreen.gd
@@ -1,37 +1,18 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-func goToMainMenu():
-	#Condition
-	if (global.accountType == "Teacher"|| global.accountType == "Admin"): #If account type is teacher or admin
-		get_tree().change_scene("res://View/Screens_Randy/MainMenuTeachers.tscn")
-	else:#If account type is student
-		get_tree().change_scene("res://View/Screens_Randy/MainMenu.tscn")
+	pass
 
 func _on_Button_pressed():
 	$FadeIn.show()
 	$FadeIn.fade_in()
 
-
-
+#Change volume func
 func _on_HSlider_value_changed(value):
 	AudioServer.set_bus_volume_db(0, value)
 	$TextureRect/MarginContainer/MarginContainer/VBoxContainer/VBoxContainer/HBoxContainer/Label.text = str(round((value + 30) * 2))
-	#$TextureRect/MarginContainer/MarginContainer/VBoxContainer/VBoxContainer/HBoxContainer/Label.text = str(round((value + 72) / 0.78))
 	
-
-
+#Go to Others menu
 func _on_FadeIn_fade_finished():
-	goToMainMenu()
+	get_tree().change_scene("res://View/Others/OthersSelection.tscn")
